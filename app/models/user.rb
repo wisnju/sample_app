3eb3dba7e561641f1a57c34063fc22df8e319318
Mingyang Wang <wangmy25@gmail.com>
2018-02-02 12:17:54 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
+    private
+
   has_many :microposts, dependent: :destroy
    validates,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ SampleApp::Application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
